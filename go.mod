9137e4d7c0cba44664b708b7bca8ac6435838140
幻音い <i@acgxt.com>
2025-03-17 11:53:16 +0800

    feat: xnumber max min

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
 )
--- standard/xnumber/math.go
+++ standard/xnumber/math.go
@@ -1,6 +1,7 @@
 package xnumber
 
 import (
+	"golang.org/x/exp/constraints"
 	"math"
 )
 
@@ -8,4 +9,34 @@ import (
 func RoundFloat(f float64, n int) float64 {
 	n10 := math.Pow10(n)
 	return math.Trunc((f+0.5/n10)*n10) / n10
+}
+
+// Max 获取最大的数值
+func Max[T constraints.Ordered](slice []T) T {
+	var zero T
+	if len(slice) == 0 {
+		return zero
+	}
+	var v = slice[0]
+	for _, item := range slice {
+		if item > v {
+			v = item
+		}
+	}
+	return v
+}
+
+// Min 取最小值
+func Min[T constraints.Ordered](slice []T) T {
+	var zero T
+	if len(slice) == 0 {
+		return zero
+	}
+	var v = slice[0]
+	for _, item := range slice {
+		if item < v {
+			v = item
+		}
+	}
+	return v
 }
